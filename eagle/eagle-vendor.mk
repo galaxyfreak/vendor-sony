f2c40bd629d74dbde9c231d44dd99f9f376c57e1
erikcas <erikcas1972@gmail.com>
2015-01-01 22:07:09 +0100

    add some camera blobs
    Change-Id: Icae6ecc3c60488b1ff4db3cf173b76fe8f7aa80c

--- eagle/eagle-vendor.mk
+++ eagle/eagle-vendor.mk
@@ -1,4 +1,5 @@
 PRODUCT_PLATFORM := yukon
 $(call inherit-product, vendor/qcom/prebuilt/qcom-vendor.mk)
 $(call inherit-product, vendor/sony/yukon/yukon-partial.mk)
+$(call inherit-product, vendor/sony/eagle/eagle-vendor-blobs.mk)
 $(call inherit-product, $(LOCAL_PATH)/eagle-partial.mk)
