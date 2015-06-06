d30506542401773faf22cc0a97c32a6faf146482
Erik Castricum <erikcas1972@gmail.com>
2015-06-07 00:06:57 +0200

    amami/rhine: add cam prop blobs

--- amami/amami-vendor.mk
+++ amami/amami-vendor.mk
@@ -2,3 +2,5 @@ PRODUCT_PLATFORM := rhine
 $(call inherit-product, vendor/qcom/prebuilt/qcom-vendor.mk)
 $(call inherit-product, vendor/sony/rhine/rhine-partial.mk)
 $(call inherit-product, $(LOCAL_PATH)/amami-partial.mk)
+$(call inherit-product, $(LOCAL_PATH)/amami-vendor-blobs.mk)
+$(call inherit-product, vendor/sony/rhine/rhine-vendor-blobs.mk)
